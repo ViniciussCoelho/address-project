c73c1294a19c56ea5c8adb894bf881750cb4545b
Vinicius Coelho <vinicius2001@gmail.com>
2023-10-03 11:40:16 -0300

    feat: add destroy user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,14 +5,20 @@ module Users
     include RackSessionsFix
     respond_to :json
 
+    def destroy
+      current_user.destroy
+  
+      render json: { status: { code: 200, message: 'User deleted successfully.' } }, status: :ok
+    end
+
     private
 
     def respond_with(current_user, _opts = {})
       if resource.persisted?
         render json: {
-          status: { code: 200, message: 'Signed up successfully.' },
+          status: { code: 201, message: 'Signed up successfully.' },
           data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
-        }
+        }, status: :created
       else
         render json: {
           status: { message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}" }
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,19 @@
 
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
-                                 sign_in: 'login',
-                                 sign_out: 'logout',
-                                 registration: 'signup'
-                               },
-                     controllers: {
-                       sessions: 'users/sessions',
-                       registrations: 'users/registrations'
-                     }
+                                  sign_in: 'login',
+                                  sign_out: 'logout',
+                                  registration: 'signup'
+                                  
+                                },
+                      controllers: {
+                        sessions: 'users/sessions',
+                        registrations: 'users/registrations'
+                      }
+
+  devise_scope :user do
+    delete 'delete_user', to: 'users/registrations#destroy'
+  end
 
   resources :contacts
 end
-
